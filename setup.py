c65755c7a9520ed5fbd2da29b74153ceb406f23f
5191032 <m274289@gmail.com>
2021-02-16 15:07:11 +0900

    ファイル名の改名 Boardという共通の基底クラスの作成

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="AnalogGames",
+    name="PyAnalogGames",
     version="0.1.5",
     author="MT-0624",
     author_email="5191032@st.hsc.ac.jp",
     description="board game library in use!!",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cm-hirano-shigetoshi/python_sample_command",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
